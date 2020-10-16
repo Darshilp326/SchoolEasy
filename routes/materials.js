178f87ed133a63b153badd8338e1baed2a812b4b
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-10-17 02:17:51 +0530

    Added students and teachers to standards

--- routes/materials.js
+++ routes/materials.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {materialController}=require('../controllers')
 
-router.route('/add').post(subjectController.addSubject);
-router.route('/:standard/all').get(subjectController.getAllSubjects)
+//router.route('/add').post(subjectController.addSubject);
+//router.route('/:standard/all').get(subjectController.getAllSubjects)
 
 module.exports=router;
