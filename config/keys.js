9055aab778aa08a873a2e8a7547570062054144e
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-10-18 23:41:48 +0530

    Changed .gitignore

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoDB:{
-        dbURL: 'mongodb+srv://darshil:12345@cluster0-9434z.mongodb.net/test?retryWrites=true&w=majority'
-    },
-    JWT:{
-     jwt_token:'schooleasy'
-    }
-};
